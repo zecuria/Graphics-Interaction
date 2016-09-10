03ce7b9160afaa6ee5da3daab2dc910ae0d884fa
zecuria <hsh>
2016-09-10 20:04:41 +1000

    Terrain is there to test camera

---
+++ Assets/DiamondSquare.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiamondSquare : MonoBehaviour {
+    public float[,] heights;
+    public int size;
+    public DiamondSquare(int size)
+    {
+        this.heights = new float[size, size];
+
+        this.heights[0, 0] = 0;
+        this.heights[0, size-1] = 0;
+        this.heights[size-1, 0] = 0;
+        this.heights[size-1, size-1] = 0;
+    }
+
+    public void PerformDiamond()
+    {
+
+    }
+
+    public void PerformSquare()
+    {
+
+    }
+}
